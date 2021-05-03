4f73f0d69c5b9487df7ace19abbf65e8c85a3a46
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-05-03 20:28:25 +0000

    working on bag template css

--- bag/contexts.py
+++ bag/contexts.py
@@ -8,14 +8,21 @@ def bag_contents(request):
     total = 0
     product_count = 0
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    grand_total = delivery + total
+    max_delivery_fee = 60
+
+    if delivery < max_delivery_fee:
+        grand_total = total + delivery
+    else:
+        grand_total = total + max_delivery_fee
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
+        'max_delivery_fee': max_delivery_fee,
         'grand_total': grand_total,
+        
     }
 
     return context
--- tadig_restaurant/settings.py
+++ tadig_restaurant/settings.py
@@ -160,3 +160,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STANDARD_DELIVERY_PERCENTAGE = 10
+MAX_DELIVERY_FEE = 60
