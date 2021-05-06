8ae5da910a2c1bf6d5df107017aeb4516015229b
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-05-06 09:09:16 +0000

    added context proccesser

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,22 +9,35 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    max_delivery_fee = 60
+    bag = request.session.get('bag', {})
 
-    if delivery < max_delivery_fee:
-        grand_total = total + delivery
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
-        grand_total = total + max_delivery_fee
-
+        delivery = 0
+        free_delivery_delta = 0
+    
+    grand_total = delivery + total
+    
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'max_delivery_fee': max_delivery_fee,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-        
     }
 
     return context
