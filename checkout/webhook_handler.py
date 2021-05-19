63d8cfe08312abc860cda32130f168899f232fb1
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-05-19 18:27:30 +0000

    updated webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -39,12 +40,26 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_full_name = shipping_details.phone
+                profile.default_phone_number = shipping_details.phone
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_street_address = shipping_details.address.line1
+                profile.default_town_or_city = shipping_details.address.city
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
+                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     postcode__iexact=shipping_details.address.postal_code,
@@ -76,12 +91,12 @@ class StripeWH_Handler:
                     original_bag=bag,
                     stripe_pid=pid,
                 )
-                for item_id in json.loads(bag).items():
+                for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     order_line_item = OrderLineItem(
                         order=order,
                         product=product,
-                        quantity=quantity,
+                        quantity=item_data,
                     )
                     order_line_item.save()
             except Exception as e:
