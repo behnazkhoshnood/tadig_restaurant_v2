63da99a50d9abbd5bf791e360edae6d6b3f1d345
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-05-20 19:39:17 +0000

    fixed the product image urls and carousel btn style

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -65,7 +65,7 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_full_name = shipping_details.phone
+                profile.default_full_name = shipping_details.name
                 profile.default_phone_number = shipping_details.phone
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_street_address = shipping_details.address.line1
