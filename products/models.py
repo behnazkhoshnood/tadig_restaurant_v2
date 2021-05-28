bd4cfcb8467d824c169016768828e5002f5e9136
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-05-28 14:14:41 +0000

    fixing some style

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from profiles.models import UserProfile
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -11,7 +11,6 @@ class Category(models.Model):
     friendly_name = models.CharField(
         max_length=254, null=True, blank=True
         )
-    slug= models.SlugField(max_length=225)
 
     def __str__(self):
         return self.name
@@ -22,11 +21,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(
-        'Category', related_name='products', null=True, blank=True, on_delete=models.SET_NULL
+        'Category', null=True, blank=True,
+        on_delete=models.SET_NULL
         )
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    slug= models.SlugField(max_length=225)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -37,12 +36,14 @@ class Product(models.Model):
 
 
 class Review(models.Model):
+    id = models.BigAutoField(
+        auto_created=True, primary_key=True,
+        serialize=False, verbose_name='ID'
+    )
     product = models.ForeignKey(
-        Product, related_name='reviews', on_delete=models.CASCADE
-    )
-    pub_date = models.DateTimeField(auto_now_add=True)
-    user_name = models.ForeignKey(
-        UserProfile, related_name='reviews', on_delete=models.CASCADE
-    )
-    comment = models.TextField(max_length=200, null=True, blank=True)
-    rating = models.IntegerField(null=True)
+        Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.id
--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-
+from .forms import ReviewForm, ProductForm
 from .models import Product, Category, Review
 
 
@@ -69,19 +69,12 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    # reviews = Review.objects.filter(product=product_id)
 
-    # add review
-
-    if request.method == 'POST' and request.user.is_authenticated:
-        rating = request.POST.get('rating', 3)
-        comment = request.POST.get('commnet', '')
-
-        review = Review.objects.create(product=product, user=request.user, rating=rating, comment=comment)
-
-        context = {
-            'product': product,
-            'review': review,
-        }
+    context = {
+        'product': product,
+        # 'reviews': reviews,
+    }
 
     return render(request, 'products/product_detail.html', context)
 
@@ -158,3 +151,70 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
+
+
+@login_required
+def add_review(request, product_id):
+    """ A view to add review to a product"""
+
+    product = Product.objects.get(pk=product_id)
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST or None)
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.comment = request.POST["comment"]
+            data.user = request.user
+            data.product = product
+            data.save()
+            messages.success(request, 'Review submitted!')
+            return redirect('product_detail', product_id)
+        else:
+            messages.error(
+                request,
+                'Failed to submit review. Please ensure the form is valid.')
+    else:
+        form = ReviewForm()
+        template = 'products/product_detail.html'
+    return render(request, template, {"form": form})
+
+
+@login_required
+def edit_review(request, product_id, review_id):
+    """A view to edit product reviews"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    review = get_object_or_404(Review, product=product, pk=review_id)
+
+    if request.user == review.user:
+        if request.method == "POST":
+            form = ReviewForm(request.POST, instance=review)
+            if form.is_valid():
+                data = form.save(commit=False)
+                data.save()
+                messages.success(request, 'Review updated!')
+                return redirect('product_detail', product_id)
+            else:
+                messages.error(
+                    request,
+                    'Failed to update review. Please ensure the form is valid.')
+                return redirect('product_detail', product_id)
+        else:
+            form = ReviewForm(instance=review)
+            return render(request, 'products/editreview.html', {"form": form})
+    else:
+        return redirect('product_detail', product_id)
+
+
+@login_required
+def delete_review(request, product_id, review_id):
+    """A view to delete product review"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    review = get_object_or_404(Review, product=product, pk=review_id)
+
+    if request.user == review.user:
+        review.delete()
+        messages.success(request, 'Review delete successfully')
+
+    return redirect('product_detail', product_id)
