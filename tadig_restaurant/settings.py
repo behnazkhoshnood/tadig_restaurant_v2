0c32da8d2344794b955d38b697869a90e2ba6960
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-06-01 23:00:21 +0000

    removed secret key and set debug

--- tadig_restaurant/settings.py
+++ tadig_restaurant/settings.py
@@ -15,17 +15,17 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kd8&+a#9!w#af6#@7zw9h905lj#*xy&b#*uk=tn^f2guckr=8f'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tadig-restaurant.herokuapp.com', 'localhost']
 
